89f691b42c38691f6375567fe7fa5aa90ba431f7
Prasamsha Sharma <prasamshari@gmail.com>
2019-09-11 23:20:23 -0700

    adding Train-Schedule files

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Train-Schedule
+
+## Overview
+This is a front end app that accepts user input to add new train name, start time and frequency to schedule. Displays schedule in repeating table with next arrival based on current time and frequency of train stops. Stores Train info in Firebase for data persistence and retreives information from database on load. User can delete train information from schedule and database with click of button.
+## Instructions
+* In this app, the user can add the new train info by writing information on Train input areas. 
+* Once submitted, the information gets stored in firebase.
+* Input the time in military time with colon
+* The minutes away and next arrival will be displayed on the screen as per the frequency time input.
+
+https://sharmap1.github.io/Train-Schedule/
+
+## Screenshot
+<img src="assets/images/screenshot.jpg" width="250" height="350"> 
+
+## Installation
+```
+Git - Installing Git: Run git from terminal
+
+git clone git://github.com/sharmap1/Train-Schedule.git bash
+
+```
+## Usage
+```
+Launch project from any code editor i.e. Sublime, Atom, VS Code, etc.
+
+Open app in any supported web browser i.e. Chrome, Firefox, Safari, etc.
+```
+## Technologies used
+* Javascript
+* jQuery
+* HTML5
+* CSS3
+* Firebase
+* event.preventDefault();
+* momentJS
+* Handling errors
+
+## Contributing
+
+* Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## History
+```
+This marks the end of unit 7 where we have learned working with Client Side Storage: uses cookies, sessionStorage, and localStorage to save and reuse user data across a user’s session; Server Side Storage: saves user info to a database that can be reused across multiple devices and sessions and Firebase to accomplish this.And also covered working with Date objects in JavaScript using the Moment.js library.
+```
+## Credits
+```
+The Coding Bootcamp @ University of Washington
+
+Instructor: Joel Stedman & TA s : Nicklas Aaland, Austin Williams, Tassa
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Copyright
+Copyright (c) [2019] [Prasamsha Sharma]
---
+++ assets/javascript/app.js
@@ -0,0 +1,163 @@
+// web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD2sj_iPzt53XQtuG4nAFakDbfTlTjTqh4",
+    authDomain: "train-schedule-1468b.firebaseapp.com",
+    databaseURL: "https://train-schedule-1468b.firebaseio.com",
+    projectId: "train-schedule-1468b",
+    storageBucket: "",
+    messagingSenderId: "850048373276",
+    appId: "1:850048373276:web:8310980869267ea7b5c5d9"
+  };
+  // 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Create a variable to reference the database
+var database =firebase.database();
+// Initial Values
+var trainName = "";
+var trainDestination = "";
+var firstTrainTime = "";
+var trainFrequency = "";
+var nextArrival = "";
+var minutesAway = "";
+
+function nowTime() {
+  var now = moment().format('LT');
+  $("#nowTime").html(now);
+  setTimeout(nowTime, 1000);
+};
+// Capture Button Click
+$("#add-button").on("click", function(event){
+    event.preventDefault();
+    // Code in the logic for storing and retrieving the most recent trains.
+    var cTrain = $("#train-name").val().trim();
+    var cTrainDestination = $("#train-destination").val().trim();
+    var cTrainTime = $("#train-time").val().trim();
+    var cTrainFreq = $("#train-freq").val().trim();
+  
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#train-time").val("");
+    $("#train-freq").val("");
+    // Code for handling the push
+    database.ref().push({
+    name: cTrain,
+    destination: cTrainDestination,
+    time: cTrainTime,
+    frequency: cTrainFreq,
+    arrival: nextArrival,
+    minutes: minutesAway,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });    
+    //  alert that train was added
+    alert("Train successuflly added!");
+
+    var timeInput = " ";
+    if (!timeInput.includes(":")) {
+        alert("Military time requires a colon to separate hours and minutes!");
+        return;
+      }
+      console.log(timeInput)
+    // split string on colon
+    var array = timeInput.split();
+    var result = {
+        hours: array[0],
+        minutes: array[1],
+    }
+
+    // check if result is valid military time
+    var isHoursOutOfBounds = result.hours > 23;     
+    var isMinutesOutOfBounds = result.minutes > 59;  
+     if (isHoursOutOfBounds || isMinutesOutOfBounds) {
+        var bothOutOfBounds = isHoursOutOfBounds && isMinutesOutOfBounds;
+        if (bothOutOfBounds)
+            alert(`${result.hours} and ${result.minutes} are larger amounts than military time!`);
+        else
+              alert(`${isHoursOutOfBounds ? result.hours : result.minutes} is larger than military time!`);
+
+        return;
+    }
+    //  empty form once submitted
+    cTrain.val("");
+    cTrainDestination.val("");
+    cTrainTime.val("");
+    cTrainFreq.val("");
+  });
+// Firebase watcher + initial loader 
+database.ref().on("child_added", function(childSnapshot) {
+    //  create local variables to store the data from firebase
+    var trainDiff = 0;
+    var trainRemainder = 0;
+    var minutesTillArrival = "";
+    var nextTrainTime = "";
+    var frequency = childSnapshot.val().frequency;
+
+    // compute the difference in time from 'now' and the first train using UNIX timestamp, store in var and convert to minutes
+    trainDiff = moment().diff(moment.unix(childSnapshot.val().time), "minutes");
+    // get the remainder of time by using 'moderator' with the frequency & time difference, store in var
+    trainRemainder = trainDiff % frequency;
+    // subtract the remainder from the frequency, store in var
+    minutesTillArrival = frequency - trainRemainder;
+    // add minutesTillArrival to now, to find next train & convert to standard time format
+    nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+    // full list of items to the well
+    $("#table-data").append("<tr><td>" + childSnapshot.val().name + "</td>" +
+    "<td>" + childSnapshot.val().destination + "</td>" +
+    "<td>" + childSnapshot.val().frequency + "</td>" +
+    "<td>" + minutesTillArrival + "</td>" +
+    "<td>" + nextTrainTime + "  " + 
+    "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+    );
+
+    $("span").hide();
+
+    // Hover view of delete button
+    $("tr").hover(
+        function() {
+            $(this).find("span").show();
+        },
+        function() {
+            $(this).find("span").hide();
+        });
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+//   // function to call the button event, and store the values in the input form
+// var storeInputs = function(event) {
+//   // prevent from from reseting
+//   event.preventDefault();
+//   // get & store input values
+//   trainName = cTrain.val().trim();
+//   trainDestination = cTrainDestination.val().trim();
+//   trainTime = moment(cTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+//   trainFrequency = cTrainFreq.val().trim();
+
+// };
+
+// // Calls storeInputs function if submit button clicked
+// $("add-button").on("click", function(event) {
+//   // form validation - if empty - alert
+//   if (cTrain.val().length === 0 || cTrainDestination.val().length === 0 || cTrainTime.val().length === 0 || cTrainFreq === 0) {
+//       alert("Please Fill All Required Fields");
+//   } else {
+//       // if form is filled out, run function
+//       storeInputs(event);
+//   }
+// });
+
+// Calls storeInputs function if enter key is clicked
+// $('form').on("keypress", function(event) {
+//   if (event.which === 13) {
+//       // form validation - if empty - alert
+//       if (cTrain.val().length === 0 || cTrainDestination.val().length === 0 || cTrainTime.val().length === 0 || cTrainFreq === 0) {
+//           alert("Please Fill All Required Fields");
+//       } else {
+//           // if form is filled out, run function
+//           storeInputs(event);
+//       }
+//   }
+// });
+  });
+
+  nowTime();
+  
